873d939c45a78cecc631798275a30155629936b1
unknown <j3-dibble@STU-CZC32353JJ.campus.ads.uwe.ac.uk>
2014-01-30 10:56:25 +0000

    Add configuration extension class

---
+++ ServiceBus.Desktop/Configuration/ServiceBusBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiceBus.Transport.Http.Configuration
+{
+    using global::ServiceBus.Configuration;
+    using System.Net.Http;
+
+    public static class ServiceBusBuilderExtensions
+    {
+        public static IServiceBusBuilder WithHttpTransport(this IServiceBusBuilder builder)
+        {
+            return builder.WithHttpTransport(new HttpClient());
+        }
+
+        public static IServiceBusBuilder WithHttpTransport(this IServiceBusBuilder builder, HttpClient client)
+        {
+            builder.WithTransport(new HttpTransporter(client));
+
+            return builder;
+        }
+    }
+}
