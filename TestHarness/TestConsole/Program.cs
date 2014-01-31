aa8c9e45a29f676fb334b6529f6ae783996318e1
James Dibble <james@jdibble.co.uk>
2014-01-31 11:33:44 +0000

    Adding test harness.

---
+++ TestHarness/TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestConsole
+{
+    using System;
+
+    using ServiceBus.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Service bus host address: ");
+
+            var hostAddress = Console.ReadLine();
+
+            Console.WriteLine("Building Service Bus");
+            
+            Console.WriteLine("Service Bus built and online.");
+
+            Console.ReadLine();
+        }
+    }
+}
