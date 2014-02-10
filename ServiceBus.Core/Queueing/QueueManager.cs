b5c7de8033c8471ca4501e159f6579a018970e2f
unknown <j3-dibble@STU-CZC32353JN.campus.ads.uwe.ac.uk>
2014-02-10 16:09:53 +0000

    DB4O for queue persistence.

--- ServiceBus.Core/Queueing/QueueManager.cs
+++ ServiceBus.Core/Queueing/QueueManager.cs
@@ -1,5 +1,6 @@
 ﻿namespace ServiceBus.Queueing
 {
+    using Db4objects.Db4o;
     using ServiceBus.Messaging;
     using System;
     using System.IO;
@@ -8,14 +9,24 @@
     {
         private bool _disposed;
 
-        
+        private readonly IObjectContainer _queuePersistence;
+
         public QueueManager(string storeDirectory)
         {
             this._disposed = false;
+
+            var databasePath = Path.Combine(storeDirectory, "queue.db40");
+
+            this._queuePersistence = Db4oEmbedded.OpenFile(databasePath);
         }
 
         public void Enqueue<TMessage>(IPeer peer, TMessage message) where TMessage : class, IMessage, new()
         {
+            var queuedMessage = new QueuedMessage<TMessage> { QueuedAt = DateTime.Now, Peer = peer, Message = message, HasSent = false };
+
+            this._queuePersistence.Store(queuedMessage);
+
+            this._queuePersistence.Commit();
         }
 
         /// <summary>
@@ -25,6 +36,7 @@
         {
             if (!this._disposed)
             {
+                this._queuePersistence.Dispose();
             }
 
             this._disposed = true;
--- ServiceBus.Core/Queueing/QueuedMessage.cs
+++ ServiceBus.Core/Queueing/QueuedMessage.cs
@@ -1,11 +1,16 @@
 ﻿namespace ServiceBus.Queueing
 {
     using ServiceBus.Messaging;
+    using System;
 
     internal class QueuedMessage<TMessage> where TMessage : class, IMessage, new()
     {
+        internal DateTime QueuedAt { get; set; }
+
         internal IPeer Peer { get; set; }
 
         internal TMessage Message { get; set; }
+
+        internal bool HasSent { get; set; }
     }
 }
